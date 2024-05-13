d683464b1a0f6a176cdcab844f02ce01dba30328
cheSau <hongquanvd1@gmail.com>
2024-05-13 23:14:25 +0700

    delete build

--- build/routes/admin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var express = require('express');
-var routerAdmin = express.Router();
-routerAdmin.get("/", function (req, res) {
-  res.send("hello admin");
-});
-module.exports.routerAdmin = routerAdmin;
